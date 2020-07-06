8b3654d9f9ff7a43ed75744229ac05ecd8e7dd03
pohleric <pohl.eric.wtal@gmail.com>
2020-07-06 17:26:14 +0200

    example script

---
+++ example_extract_HAR30.py
@@ -0,0 +1,15 @@
+from climate_data_extraction import met
+
+
+# --- ERA5 daily
+melf = met()
+melf.era5_dir = '/Users/pohle/TMP/'
+melf.output_dir = '/Users/pohle/PycharmProjects/data-download/marlene/output_ERA5_hourly/'
+melf.era5_variable = 'tp'
+melf.mstation_fname = "/Volumes/EX1/data/glacier/marlene/abramov_poly.shp"
+# melf.era5_subset = [70., 73., 37., 41]
+melf.read_ERA5_hourly(subset=False)
+melf.shapefile_ATTR_FIELD = 'OBJECTID'
+melf.read_shape()
+melf.extract_values()
+melf.save_data()
